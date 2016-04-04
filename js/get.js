1fd4507c462f9b1dd66879bad761a38d650323c2
oscar <oscar84922tw@gmail.com>
2016-04-04 20:29:36 +0800

    Object to Array Failed

--- js/get.js
+++ js/get.js
@@ -1,43 +1,65 @@
 /*
  * @Author: oscar
  * @Date:   2016-04-04 01:03:00
- * @Last Modified by:   oscar84922tw
- * @Last Modified time: 2016-04-04 13:45:31
+ * @Last Modified by:   oscar
+ * @Last Modified time: 2016-04-04 20:15:21
  */
 
-'use strict';
 
 
 
 function loadXMLDoc() {
-    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-        var xmlhttp = new XMLHttpRequest();
-    } else { // code for IE6, IE5
-        var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            // var consultJson = JSON.parse(xmlhttp.responseText);
-            // for(var i = 0;i<consultJson.)
-            var data = xmlhttp.responseText;
-            // var dataarr = $.parseJSON(data)
-            // document.getElementById("myDiv").innerHTML = dataarr[0];
-            console.log(jsonToMap(data));
-            // var arr = $.map(obj, function(el) { return el });
-            // for (var i = 0; i <= JSON.parse(data).length; i--) {
-            //     0[i]
-            // }
-            // console.log(consultJson);
-            // drawTable(JSON.parse(xmlhttp.responseText));
-        }
-    }
-    xmlhttp.open("GET", "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=5aacba65-afda-4ad5-88f5-6026934140e6", true);
-    xmlhttp.send();
+    var resultdata = new Array();
+    $.ajax({
+            url: 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=5aacba65-afda-4ad5-88f5-6026934140e6',
+            type: 'get',
+            dataType: 'json',
+            success: function(data) {
+                if (data) {
+                    $.each(data, function(i, item) {
+                        console.log(item);
+                        var inner = {
+
+                            results: {
+                                offset: data.result.offset,
+                                limit: data.result.limit,
+                                count: data.result.count,
+                                sort: data.result.sort,
+                                result: $.each(data.result.results, function(i, item) {
+                                    console.log('down2');
+                                    var dd = {
+                                        id: data.result.results._id,
+                                        sectionID: data.result.results.SectionId,
+                                        SectionName: data.result.results.SectionName,
+                                        AvgSpd: data.result.results.AvgSpd,
+                                        AvgOcc: data.result.results.AvgOcc,
+                                        TotalVol: data.result.results.TotalVol,
+                                        MOELevel: data.result.results.MOELevel,
+                                        StartWgsX: data.result.results.StartWgsX,
+                                        StartWgsY: data.result.results.StartWgsY,
+                                        EndWgsX: data.result.results.EndWgsX,
+                                        EndWgsY: data.result.results.EndWgsY,
+                                    };
+                                }),
+                            }
+                        }
+
+                        resultdata.push(inner);
+                        console.log(resultdata);
+                         return resultdata;
+                    });
+                }
+            }
+        })
+        // console.log(resultdata);
+
 }
 
-function jsonToMap(jsonStr) {
-    return new Map(JSON.parse(jsonStr));
-}
+var tmp = $.makeArray(loadXMLDoc());
+
+console.log(typeof tmp);
+
+// drawTable();
 
 function drawTable(data) {
     for (var i = 0; i < data.length; i++) {
@@ -48,6 +70,16 @@ function drawTable(data) {
 function drawRow(rowData) {
     var row = $("<tr />")
     $("#personDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData._id + "</td>"));
+    row.append($("<td>" + rowData.SectionId + "</td>"));
     row.append($("<td>" + rowData.SectionName + "</td>"));
     row.append($("<td>" + rowData.AvgSpd + "</td>"));
+    row.append($("<td>" + rowData.AvgOcc + "</td>"));
+    row.append($("<td>" + rowData.TotalVol + "</td>"));
+    row.append($("<td>" + rowData.MOELevel + "</td>"));
+    row.append($("<td>" + rowData.StartWgsX + "</td>"));
+    row.append($("<td>" + rowData.StartWgsY + "</td>"));
+    row.append($("<td>" + rowData.EndWgsX + "</td>"));
+    row.append($("<td>" + rowData.EndWgsY + "</td>"));
+
 }
