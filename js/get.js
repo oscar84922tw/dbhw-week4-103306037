3c1773881a337a79e976c2075d5be52557323945
oscar84922tw <oscar84922tw@gmail.com>
2016-04-05 22:34:56 +0800

    dataFinish

--- js/get.js
+++ js/get.js
@@ -1,12 +1,12 @@
 /*
  * @Author: oscar
  * @Date:   2016-04-04 01:03:00
- * @Last Modified by:   oscar
- * @Last Modified time: 2016-04-04 20:15:21
+ * @Last Modified by:   oscar84922tw
+ * @Last Modified time: 2016-04-05 22:34:17
  */
 
 
-
+loadXMLDoc();
 
 function loadXMLDoc() {
     var resultdata = new Array();
@@ -15,71 +15,70 @@ function loadXMLDoc() {
             type: 'get',
             dataType: 'json',
             success: function(data) {
-                if (data) {
-                    $.each(data, function(i, item) {
-                        console.log(item);
-                        var inner = {
+                var dataList = data.result.results;
+                // var WebdataList = datalist.map
+                var WebDataList = dataList.map(function(dataList) {
+                    var HTMLString = '<tr><td data-th="_id">' + dataList._id + '</td><td data-th="SectionId">' + dataList.SectionId +'</td><td data-th="SectionName">'+dataList.SectionName +'</td><td data-th="AvgSpd">'+dataList.AvgSpd+'</td><td data-th="AvgOcc">'+dataList.AvgOcc+'</td><td data-th="MOELevel">'+dataList.MOELevel+'</td><td data-th="StartWgsX">'+dataList.StartWgsX+'</td><td data-th="EndWgsX">'+dataList.EndWgsX+'</td><td data-th="StartWgsY">'+dataList.StartWgsY+'</td><td data-th="EndWgsY">'+dataList.EndWgsY+'</td></tr>';
+                    var items = "";
+                //     <tr>
+                //     <td data-th="_id"></td>
+                //     <td data-th="SectionId">03/23〔三〕 </td>
+                //     <td data-th="SectionName">$119,900</td>
+                //     <td data-th="AvgSpd">26</td>
+                //     <td data-th="AvgOcc">6</td>
+                //     <td data-th="MOELevel">6</td>
+                //     <td data-th="StartWgsX">6</td>
+                //     <td data-th="EndWgsX"></td>
+                //     <td data-th="StartWgsY">6</td>
+                //     <td data-th="StartWgsY"></td>
+                //     </tr>
+                    // for (var key in animal) {
+                    //     if (key != "_id" && key != "ImageName") {
+                    //         items = items + "<li class='list-group-item'>" + key + ":" + animal[key] + "</li>";
+                    //     }
+                    // }
+                    var HTMLString = HTMLString + items + "</ul></p><div>";
+                    return HTMLString;
+                });
+                WebDataList.forEach(function(html) {
+                    $(html).appendTo("#myDiv");
+                });
+                // if (dataList) {
+                //     $.each(dataList, function(i, item) {
+                //         console.log(Object.values(item));
+                //         // document.getElementById('myDiv').innerHTML = Object.values(item);
 
-                            results: {
-                                offset: data.result.offset,
-                                limit: data.result.limit,
-                                count: data.result.count,
-                                sort: data.result.sort,
-                                result: $.each(data.result.results, function(i, item) {
-                                    console.log('down2');
-                                    var dd = {
-                                        id: data.result.results._id,
-                                        sectionID: data.result.results.SectionId,
-                                        SectionName: data.result.results.SectionName,
-                                        AvgSpd: data.result.results.AvgSpd,
-                                        AvgOcc: data.result.results.AvgOcc,
-                                        TotalVol: data.result.results.TotalVol,
-                                        MOELevel: data.result.results.MOELevel,
-                                        StartWgsX: data.result.results.StartWgsX,
-                                        StartWgsY: data.result.results.StartWgsY,
-                                        EndWgsX: data.result.results.EndWgsX,
-                                        EndWgsY: data.result.results.EndWgsY,
-                                    };
-                                }),
-                            }
-                        }
-
-                        resultdata.push(inner);
-                        console.log(resultdata);
-                         return resultdata;
-                    });
-                }
+                //     });
+                // }
             }
         })
         // console.log(resultdata);
 
 }
 
-var tmp = $.makeArray(loadXMLDoc());
 
-console.log(typeof tmp);
 
 // drawTable();
 
-function drawTable(data) {
-    for (var i = 0; i < data.length; i++) {
-        drawRow(data[i]);
-    }
-}
+// function drawTable(data) {
+//     for (var i = 0; i < data.length; i++) {
+//         drawRow(data[i]);
+//     }
+// }
 
-function drawRow(rowData) {
-    var row = $("<tr />")
-    $("#personDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-    row.append($("<td>" + rowData._id + "</td>"));
-    row.append($("<td>" + rowData.SectionId + "</td>"));
-    row.append($("<td>" + rowData.SectionName + "</td>"));
-    row.append($("<td>" + rowData.AvgSpd + "</td>"));
-    row.append($("<td>" + rowData.AvgOcc + "</td>"));
-    row.append($("<td>" + rowData.TotalVol + "</td>"));
-    row.append($("<td>" + rowData.MOELevel + "</td>"));
-    row.append($("<td>" + rowData.StartWgsX + "</td>"));
-    row.append($("<td>" + rowData.StartWgsY + "</td>"));
-    row.append($("<td>" + rowData.EndWgsX + "</td>"));
-    row.append($("<td>" + rowData.EndWgsY + "</td>"));
+// function drawRow(rowData) {
+//     var row = $("<tr />")
+//     $("#personDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+//     row.append($("<td>" + rowData._id + "</td>"));
+//     row.append($("<td>" + rowData.SectionId + "</td>"));
+//     row.append($("<td>" + rowData.SectionName + "</td>"));
+//     row.append($("<td>" + rowData.AvgSpd + "</td>"));
+//     row.append($("<td>" + rowData.AvgOcc + "</td>"));
+//     row.append($("<td>" + rowData.TotalVol + "</td>"));
+//     row.append($("<td>" + rowData.MOELevel + "</td>"));
+//     row.append($("<td>" + rowData.StartWgsX + "</td>"));
+//     row.append($("<td>" + rowData.StartWgsY + "</td>"));
+//     row.append($("<td>" + rowData.EndWgsX + "</td>"));
+//     row.append($("<td>" + rowData.EndWgsY + "</td>"));
 
-}
+// }
